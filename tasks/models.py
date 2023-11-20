df6c4c954dd30c58ac621fc8e0e3a358f5f2f36f
felipito <felipeochoa2015@gmail.com>
2023-11-20 11:00:43 -0500

    added filters so that users can only see the task they created

--- tasks/models.py
+++ tasks/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+from django.contrib.auth.models import User
+
 class Task(models.Model):
-    #task_id = models.AutoField(primary_key=True)
-    author = models.ForeignKey("auth.User" ,on_delete=models.CASCADE, default="felipito")
+    
+    author = models.ForeignKey(User ,on_delete=models.CASCADE)
+    #author = models.ForeignKey("auth.User" ,on_delete=models.CASCADE, default="felipito")
     title = models.CharField(max_length=200)
     date = models.DateField()
     task_time = models.TimeField()
--- tasks/views.py
+++ tasks/views.py
@@ -3,52 +3,59 @@ from django.urls import reverse, reverse_lazy
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
+from django.contrib.auth.mixins import LoginRequiredMixin 
+
 from .models import Task
 
+
+
 # Create your views here.
-class TaskDeleteView(DeleteView):
+class TaskDeleteView(LoginRequiredMixin, DeleteView):
     model = Task
     template_name = "task_delete.html"
     #we use reverse_lazy here as opposed to just reverse so that it won’t execute
     #the URL redirect until our view has finished deleting the task.
     success_url = reverse_lazy("tasks")
 
-class TaskCreateView(CreateView):  
+class TaskCreateView(LoginRequiredMixin, CreateView):  
     model = Task
     template_name = "new_task.html"
-    fields = ["title", "author", "date", "task_time", "details"]
+    #fields = ["title", "author", "date", "task_time", "details"]
 
-class TaskUpdateView(UpdateView):
+    fields = ["title", "date", "task_time", "details"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class TaskUpdateView(LoginRequiredMixin, UpdateView):
     model = Task
     template_name = "task_edit.html"
     fields = ["title", "date", "task_time", "details"]
     
  
 
-
-class TaskDetailView(DetailView):
+class TaskDetailView(LoginRequiredMixin, DetailView):
     model = Task
     template_name = "task_detail.html"
     context_object_name = 'task'
 
 
-class TaskPageView(ListView):
+class TaskPageView(LoginRequiredMixin, ListView):
     model = Task
     template_name = "tasks.html"
     context_object_name = 'task_list'
    
-
     def get_queryset(self): 
-        return Task.objects.filter(is_completed = False)
+        return Task.objects.filter(author=self.request.user, is_completed=False)
 
-class TaskCompletedView(ListView):
+class TaskCompletedView(LoginRequiredMixin, ListView):
     model = Task
     template_name = "completed_tasks.html"
     context_object_name = "completed_tasks"
 
-
     def get_queryset(self):
-        return Task.objects.filter(is_completed = True)
+        return Task.objects.filter(author=self.request.user, is_completed=True)
          
 def mark_completed(request, pk):
     task = Task.objects.get(pk=pk)
