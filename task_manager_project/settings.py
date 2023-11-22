c04078d0d631aa1ae993884665f5bf90ce53785f
felipito <felipeochoa2015@gmail.com>
2023-11-22 13:42:30 -0500

    settings update

--- task_manager_project/settings.py
+++ task_manager_project/settings.py
@@ -151,4 +151,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'taskemailmanager@gmail.com'
-EMAIL_HOST_PASSWORD = 'keuj vksh nqht nmnd'
+EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD")
